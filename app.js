61e03463f73f3cfa935419f4a17df22bc4369e08
NAZIK-Dairbekovna <nazikdairbekova2@gmail.com>
2023-06-04 23:11:22 +0600

    ss

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ function setTime(value) {
 
 function finishGame() {
     timeEl.parentNode.classList.add('hide')
-    board.innerHTML = `<h1>Count: <span class="primary">${score}</span></h1>`
+    board.innerHTML = `<div class="restart"><h1>Count: <span class="primary">${score}</span></h1><button class="start" id="start">START</button></div>`
 }
 
 function createRandomCircle() {
